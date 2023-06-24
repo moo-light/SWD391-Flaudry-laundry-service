4f1d57be737ddf45478a5e9eab74a59f475b73db
ducanhnt22 <ducanhbon4@gmail.com>
2023-06-25 01:25:32 +0700

    a

---
+++ Apis/Application/ViewModels/FilterModels/OrderDetailFilteringModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.FilterModels
+{
+    public class OrderDetailFilteringModel : BaseFilterringModel
+    {
+        public Guid?[]? OrderId { get; set; }
+        public Guid?[]? ServiceId { get; set; }
+        public string?[]? Weight { get; set; }
+    }
+}
